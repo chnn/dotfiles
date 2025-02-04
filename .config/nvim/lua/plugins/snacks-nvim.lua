0927829785980f4dbb26816525754e6ae6f4417c
Christopher Henn <chris@chrishenn.net>
2025-02-04 13:36:44 -0500

    Use snacks picker

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -5,5 +5,58 @@ return {
   opts = {
     bigfile = { enabled = true },
     notifier = { enabled = true },
+    picker = {
+      layout = "ivy_split",
+      formatters = {
+        file = { truncate = 80 },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<ESC>"] = { "close", mode = { "n", "i" } },
+            ["<C-u>"] = { "<C-o>cc", mode = { "i" }, expr = true },
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = "Open file picker",
+    },
+    {
+      "<leader>b",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Open buffer picker",
+    },
+    {
+      "<leader>d",
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = "Open diagnostics picker",
+    },
+    {
+      "<leader>/",
+      function()
+        Snacks.picker()
+      end,
+      desc = "Open pickers picker",
+    },
+    {
+      "<leader>,",
+      function()
+        Snacks.picker.files({
+          dirs = { "~/.config/nvim" },
+        })
+      end,
+      desc = "Edit config",
+    },
   },
 }
