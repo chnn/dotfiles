e9dcde608e5cfee9758f16722ddc95418c0e6810
Christopher Henn <chris@chrishenn.net>
2025-06-04 14:53:54 -0700

    Update Zed, nvim

--- .config/nvim/lua/plugins/minuet-ai-nvim.lua
+++ .config/nvim/lua/plugins/minuet-ai-nvim.lua
@@ -5,9 +5,13 @@ return {
   },
   config = function()
     require("minuet").setup({
-      provider = "gemini",
+      provider = "claude",
       provider_options = {
-        gemini = { model = "gemini-2.0-flash" },
+        claude = { model = "claude-opus-4-20250514" },
+        gemini = {
+          -- model = "gemini-2.5-flash-preview-05-20",
+          model = "gemini-2.5-pro-preview-05-06",
+        },
       },
     })
   end,
--- .config/nvim/lua/plugins/tinted-vim.lua
+++ .config/nvim/lua/plugins/tinted-vim.lua
@@ -2,8 +2,8 @@ return {
   "tinted-theming/tinted-vim",
   config = function()
     vim.o.termguicolors = true
-    vim.o.background = "light"
-    vim.cmd.colorscheme("base16-humanoid-light")
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("base16-default-dark")
     vim.api.nvim_set_hl(0, "FlashLabel", { link = "lualine_a_normal" })
   end,
 }
