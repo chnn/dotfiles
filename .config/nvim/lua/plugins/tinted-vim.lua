0e9430635f98f6460f94e1bcb98c2694eb80ae29
Christopher Henn <chris@chrishenn.net>
2025-06-13 15:06:45 -0400

    Add Ghostty config and change colors

--- .config/nvim/lua/plugins/tinted-vim.lua
+++ .config/nvim/lua/plugins/tinted-vim.lua
@@ -2,8 +2,8 @@ return {
   "tinted-theming/tinted-vim",
   config = function()
     vim.o.termguicolors = true
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("base16-default-dark")
+    vim.o.background = "light"
+    vim.cmd.colorscheme("base16-one-light")
     vim.api.nvim_set_hl(0, "FlashLabel", { link = "lualine_a_normal" })
   end,
 }
