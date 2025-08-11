964de755a94e936a701089d5de5d265a55fef0c1
Christopher Henn <chris@chrishenn.net>
2025-08-11 10:15:49 -0400

    Use melange colors

--- .config/nvim/lua/plugins/tinted-vim.lua
+++ .config/nvim/lua/plugins/tinted-vim.lua
@@ -1,9 +1,6 @@
 return {
   "tinted-theming/tinted-vim",
   config = function()
-    vim.o.termguicolors = true
-    vim.o.background = "light"
-    vim.cmd.colorscheme("base16-one-light")
-    vim.api.nvim_set_hl(0, "FlashLabel", { link = "lualine_a_normal" })
+    -- vim.cmd.colorscheme("base16-one-light")
   end,
 }
